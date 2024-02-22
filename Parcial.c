3018d8ef27976c0b96d24ec14d771ee5d0114864
thezulux24 <thezulux24@gmail.com>
2024-02-21 19:58:27 -0500

    finalizado

--- Parcial.c
+++ Parcial.c
@@ -37,7 +37,7 @@ int main() {
     int flag2 = 0;
     char *nombre= NULL; ;
     size_t tamano = 0;
-    printf("ingresa tu nombre");
+    printf("ingresa tu nombre: ");
     getline(&nombre, &tamano, stdin);
     while (vidaJugador > 0 && vidaEnemigo > 0){
         atack = 0, defense = 0, energia = 3, tamanoDinamico = 5;
@@ -61,42 +61,44 @@ int main() {
             deckUsable[i]= deck[i];
         }
 
-        for (int i=0; i<tamanoDinamico; i++){
-            opcion = deckUsable[i];
-        switch(opcion) {
-            case 1:
-                printf("-> Puedes elegir [1]Ataque\n AT= %d  EN= %d\n", cartas[0][0],cartas[0][3]);
-                break;
-            case 2:
-                printf("-> Puedes elegir [2]Defensa\n DF= %d  EN= %d\n", cartas[1][1],cartas[1][3]);
-                break;
-            case 3:
-                printf("-> Puedes elegir [3]Ultima sangre\n AT= %d  LF=%d  EN= %d\n", cartas[2][0],cartas[2][2],cartas[2][3]);
-                break;
-            case 4:
-                printf("-> Puedes elegir [4]milagro\n ME=%d   EN=%d\n", cartas[3][4],cartas[3][3] );
-                break;
-            case 5:
-                printf("-> Puedes elegir [5]resplandor\n AT= %d EN= %d\n", cartas[4][0],cartas[4][3]);
-                break;
-            case 6:
-                printf("-> Puedes elegir [6]desvio\n DF= %d  EN= %d\n", cartas[5][1],cartas[5][3]);
-                break;
-            case 7:
-                printf("-> Puedes elegir [7]rebote\n AT= %d DF= %d EN= %d\n", cartas[6][0], cartas[6][1],cartas[6][3]);
-                break;        
-            }
-        }
+        
         while (termino != 1 ){
-            printf("\n");
-            printf("por favor seleccione su carta, o escriba 0 para terminar finalizar turno\n");
-            printf("\n ----------------- \n");
-            printf("");
-            printf("Hola, %s , tú vida es: %d/%d\n", nombre, vidaJugador, vidaJugador2);
+
+            printf("Hola, %s tú vida es: %d/%d\n", nombre, vidaJugador, vidaJugador2);
             printf("su energia es: %d\n", energia);
             printf("Su enemigo se llama Kratos, su vida es: %d/%d\n", vidaEnemigo, vidaEnemigo2);
             printf("su enemigo le hara: %d dano\n", ataqueEnemigo);
             printf("----------------- \n");
+            printf("\n Las cartas disponibles son: \n");
+            for (int i=0; i<tamanoDinamico; i++){
+            opcion = deckUsable[i];
+            switch(opcion) {
+                case 1:
+                    printf("-> Puedes elegir [1]Ataque\n AT= %d  EN= %d\n", cartas[0][0],cartas[0][3]);
+                    break;
+                case 2:
+                    printf("-> Puedes elegir [2]Defensa\n DF= %d  EN= %d\n", cartas[1][1],cartas[1][3]);
+                    break;
+                case 3:
+                    printf("-> Puedes elegir [3]Ultima sangre\n AT= %d  LF=%d  EN= %d\n", cartas[2][0],cartas[2][2],cartas[2][3]);
+                    break;
+                case 4:
+                    printf("-> Puedes elegir [4]milagro\n ME=%d   EN=%d\n", cartas[3][4],cartas[3][3] );
+                    break;
+                case 5:
+                    printf("-> Puedes elegir [5]resplandor\n AT= %d EN= %d\n", cartas[4][0],cartas[4][3]);
+                    break;
+                case 6:
+                    printf("-> Puedes elegir [6]desvio\n DF= %d  EN= %d\n", cartas[5][1],cartas[5][3]);
+                    break;
+                case 7:
+                    printf("-> Puedes elegir [7]rebote\n AT= %d DF= %d EN= %d\n", cartas[6][0], cartas[6][1],cartas[6][3]);
+                    break;        
+                }
+            }
+            printf("\n");
+            printf("por favor seleccione su carta, o escriba 0 para terminar finalizar turno\n");
+            printf("\n ----------------- \n");
             
             scanf("%d", &seleccion);
             if (seleccion == 0 ){
@@ -104,7 +106,7 @@ int main() {
                 printf("\n ----------------- \n");
                 printf("turno finalizado\n");
             }
-            else{
+            else if (energia - cartas[seleccion-1][3] >= 0){
                 for (int i = 0; i < tamanoDinamico ; i++){
                     if (deckUsable[i] == seleccion ){
                         flag2 = 1;            
@@ -118,7 +120,7 @@ int main() {
                 if (flag2 == 1){
                     tamanoDinamico -= 1;
                     deckUsable = (int*)realloc(deckUsable, tamanoDinamico*size);
-                    if (energia - cartas[seleccion-1][3] >= 0){
+                    
                         atack += cartas[seleccion-1][0];
                         defense += cartas[seleccion-1][1];
                         energia -= cartas[seleccion-1][3];
@@ -128,10 +130,8 @@ int main() {
                         printf("has generado %d dano a tu enemigo\n", atack);
                         vidaEnemigo -= atack;
                         atack = 0;
-                    }
-                    else {
-                        printf("¡NO TIENES ENERGIA SUFICIENTE!\n");
-                    }
+                    
+
                 }
                 else if (flag2 == 0){
                     printf("¡LA CARTA YA NO ESTA EN EL DECK!\n");
@@ -140,7 +140,12 @@ int main() {
                 flag2 = 0;
                 
             }
+            else {
+            printf("¡NO TIENES ENERGIA SUFICIENTE!\n");
+            }
         }
+
+        // termina el ciclo
         if (ataqueEnemigo > defense){
             vidaJugador -= defense + ataqueEnemigo;
             printf("el enemigo ha generado %d dano\n", ataqueEnemigo-defense);
